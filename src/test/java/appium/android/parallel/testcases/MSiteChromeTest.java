0eb1d1b998dfe319d5b8975a5122950bb78d5474
Ketan Sethi <ketansethi786@gmail.com>
2018-09-19 22:12:42 +0530

    test

--- src/test/java/appium/android/parallel/testcases/MSiteChromeTest.java
+++ src/test/java/appium/android/parallel/testcases/MSiteChromeTest.java
@@ -1,5 +1,6 @@
 package appium.android.parallel.testcases;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.By;
@@ -12,17 +13,26 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 
 public class MSiteChromeTest {
 	public AndroidDriver<AndroidElement> driver;
 
 	@BeforeTest
-	public void testMethod() {
+	public void setUp() throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		capabilities.setCapability("platformVersion", "8.1");
-		capabilities.setCapability("deviceName", "samsung_galaxy_s6_8.1");
+		// #######EMULATOR MOBILE WEB#################################
+//		capabilities.setCapability("platformVersion", "8.1");
+//		capabilities.setCapability("deviceName", "samsung_galaxy_s6_8.1");
+//		capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.android.chrome");
+
+		// #######REAL DEVICE MOBILE WEB#################################
+		capabilities.setCapability("platformVersion", "7.0");
+		capabilities.setCapability("deviceName", "464b5c510204");
 		capabilities.setCapability("browserName", "chrome");
+
+		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.google.android.apps.chrome.Main");
 
 		try {
 			driver = new AndroidDriver<>(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
@@ -30,7 +40,6 @@ public class MSiteChromeTest {
 			WebElement element = driver.findElement(By.name("q"));
 			element.sendKeys("AppiumConf");
 			element.submit();
-
 			driver.findElement(By.id("ires")).isDisplayed();
 		} catch (Exception e) {
 			e.printStackTrace();
